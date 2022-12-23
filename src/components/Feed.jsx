08efdde8e7a5fce050693f11f50131a1103330cf
Waldemar Głaz <w.glaz@cheil.com>
2022-12-23 08:58:12 +0100

    fetchFromApi

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPi";
 
 function Feed() {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    const data = fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then(
+      (data) => {
+        setVideos(data.items);
+      }
+    );
+  }, [selectedCategory]);
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -12,7 +22,10 @@ function Feed() {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyright"
           variant="body2"
@@ -20,6 +33,17 @@ function Feed() {
         >
           Copyright 2022 JSM Media
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "hidden", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          mb={2}
+          sx={{ color: "white" }}
+          fontWeight="bold"
+        >
+          {selectedCategory} <span style={{ color: "#f31503" }}>Videos</span>
+        </Typography>
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
---
+++ src/components/Videos.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+const Videos = ({ videos }) => {
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>{item.id.videoId && <VideoCard video={item} />}</Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
